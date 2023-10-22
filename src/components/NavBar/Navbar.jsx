db541d8eb72e818327405503d0c732389cc5987f
Shamil Roshan <shamilroshan390@gmail.com>
2023-10-22 17:34:00 +0530

    Made the navabar resonsive

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,14 +1,15 @@
 import './Navbar.css'
-import React, { useState , useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Navbar() {
 
-    const [ small , changeSmall ] = useState( false )
+    const [small, changeSmall] = useState(false)
+    const [responsiveNav, visibleresponsiveNav] = useState(false)
 
-    useEffect( () => {
+    useEffect(() => {
 
         winidowWidth()
-        window.addEventListener( 'resize' , winidowWidth)
+        window.addEventListener('resize', winidowWidth)
 
         return () => {
 
@@ -16,14 +17,14 @@ function Navbar() {
 
         }
 
-    } , [] )
+    }, [])
 
     const winidowWidth = () => {
 
         const width = window.innerWidth
         console.log(width);
-        if ( width <= 767 ) changeSmall( true )
-        else changeSmall( false )
+        if (width <= 767) changeSmall(true)
+        else changeSmall(false)
 
     }
 
@@ -32,10 +33,15 @@ function Navbar() {
 
             <div className="left-section">
 
-                <img className={ small ? 'netflix-N-logo' : 'netflix-logo' } src={ small ? "https://upload.wikimedia.org/wikipedia/commons/3/35/Nficon2016.png?" : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" } alt="logo" />
-                <div className="left-lists">
+                <img onClick={(e) => {
 
-                    <ul className='menu' >
+                    if (responsiveNav) visibleresponsiveNav(false, changeSmall(true))
+                    else if (e.target.className === 'netflix-N-logo') visibleresponsiveNav(true, changeSmall(false))
+
+                }} className={(small || responsiveNav) ? 'netflix-N-logo' : 'netflix-logo'} src={(small || responsiveNav) ? "https://upload.wikimedia.org/wikipedia/commons/3/35/Nficon2016.png?" : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"} alt="logo" />
+                {!small && <div className={responsiveNav ? 'responsive-left-lists' : 'left-lists'} >
+
+                    <ul className={responsiveNav ? 'responsive-left-menu' : 'menu'} >
                         <li>Home</li>
                         <li>TV Shows</li>
                         <li>Movies</li>
@@ -43,29 +49,34 @@ function Navbar() {
                         <li>My List</li>
                     </ul>
 
-                </div>
+                </div>}
 
             </div>
             <div className="right-section">
 
-                <div className="search">
+                <div className={(small || responsiveNav) ? 'responsive-search' : 'search'}>
 
                     <input type="text" placeholder='Search' />
                     <i class='bx bx-search'></i>
 
                 </div>
-                <div className="right-lists">
+                {!small && <div className={responsiveNav ? 'responsive-right-lists' : 'right-lists'}>
 
-                    <ul className='menu' >
+                    <ul className={responsiveNav ? 'responsive-right-menu' : 'menu'} >
 
                         <li>KIDS</li>
                         <li>DVD</li>
 
                     </ul>
 
-                </div>
+                </div>}
                 <i id='notification' class='bx bxs-bell'></i>
                 <img className='netflix-avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="avatar" />
+
+            </div>
+
+            <div className={responsiveNav ? "responsive-menu" : 'not-responsive-menu'}>
+
 
 
             </div>
